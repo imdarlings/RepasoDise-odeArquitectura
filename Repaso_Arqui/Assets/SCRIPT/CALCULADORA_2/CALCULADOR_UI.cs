d39412d16535f97606e4b37bf0820876db3409a3
Jesus Adrian Buñay Cepeda <dhinestroza880@soyudemedellin.edu.co>
2026-02-24 13:15:01 -0500

    TRABAJO EN CLASE

--- Repaso_Arqui/Assets/SCRIPT/CALCULADORA_2/CALCULADOR_UI.cs
+++ Repaso_Arqui/Assets/SCRIPT/CALCULADORA_2/CALCULADOR_UI.cs
@@ -3,33 +3,59 @@ using UnityEngine;
 
 public class CALCULADOR_UI : MonoBehaviour
 {
+
+    public enum Operador
+    {
+        Suma,
+        Resta,
+        Multiplicacion,
+        Division
+    }
+
+    [Header("Inputs")]
     [SerializeField] private TMP_InputField _inputA;
     [SerializeField] private TMP_InputField _inputB;
 
-    public float MOMO(string input)
-        {
-        return float.Parse(input);
+    [Header("Outputs")]
+    [SerializeField] private TMP_Text _resultado;
+
+    [Header("Config")]
+    [SerializeField] private Operador _operador;
+
+    public void SeleccionarOperador(Operador operador)
+    {
+        _operador = operador;
     }
 
-    public string Calcular(string operador, float inputA , float inputB)
+    public void OperadorSuma() => _operador = Operador.Suma;
+    public void OperadorResta()
+    {
+        _operador = Operador.Resta;
+    }
+
+    public void Calcular(string operador)
     { 
-        switch (operador)
+        float.TryParse(_inputA.text, out float a);
+        float.TryParse(_inputB.text, out float b);
+
+        switch (_operador)
         {
-            case "+":
-                return inputA + inputB). ToString();
+            case Operador.Suma:
+                _resultado.text = (a + b).ToString();
                 break;
 
-            case "-":
-                return inputA - inputB). ToString();
+            case Operador.Resta:
+                _resultado.text = (a - b).ToString();
                 break;
 
-            case "*":
-                return inputA * inputB). ToString();
+            case Operador.Multiplicacion:
+                _resultado.text = (a * b).ToString();
                 break;
 
-            case "/":
-                return inputA / inputB). ToString();
+            case Operador.Division:
+                _resultado.text = (a / b).ToString();
                 break;
+
         default:
                 Debug.Log("Si no pones un operador tienes 0");
                 break;
